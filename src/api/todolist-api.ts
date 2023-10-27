eeea1cbc87943acd31ed9793059fb160038f72d5
Faukkirik <kote_33419@mail.ru>
2023-10-27 14:26:29 +0300

    response todo response type

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -9,16 +9,28 @@ const instance = axios.create({
 })
 
 export const todolistAPI = {
-    getTodolist(){
-        return instance.get(`todo-lists`)
+    getTodolist() {
+        return instance.get<TodolistType[]>(`todo-lists`)
     },
-    createTodolist(title: string){
-        return instance.post(`todo-lists`,{title: title})
+    createTodolist(title: string) {
+        return instance.post<ResponseType<{item: TodolistType}>>(`todo-lists`, {title: title})
     },
-    deleteTodolist(todolistId: string){
-        return instance.delete(`todo-lists/${todolistId}`)
+    deleteTodolist(todolistId: string) {
+        return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
     },
-    updateTodolist(todolistId: string, newTitle: string){
-        return instance.put(`todo-lists/${todolistId}`,{title: newTitle})
+    updateTodolist(todolistId: string, newTitle: string) {
+        return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title: newTitle})
     }
+}
+type TodolistType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+type ResponseType<T = {}> = {
+    resultCode: number
+    messages: string[]
+    fieldsErrors: string[]
+    data: T
 }
